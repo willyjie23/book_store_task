a5b7795bd39833135a48a8397323284256b29ff8
willy chang <willy.chang@oakda.io>
2021-01-10 17:19:32 +0800

    List all book stores that have more or less than x number of books within a price range task done!

--- app/api/v1/book_api.rb
+++ app/api/v1/book_api.rb
@@ -22,5 +22,22 @@ module V1
 
       result.sort_by { |b| b['price'] }
     end
+
+    desc 'Serach books price and quantity'
+    params do
+      requires :price, type: Float
+      requires :quantity, type: Integer
+    end
+
+    get '/book_price_quantity' do
+      result = []
+      Store.all.each do |s|
+        total = s.books.select { |b| b['price'] <= params[:price] }
+        result << s if total.size >= params[:quantity]
+      end
+      not_found_method(result)
+
+      result.sort_by { |b| b['price'] }
+    end
   end
 end
--- spec/api/v1/book_api_spec.rb
+++ spec/api/v1/book_api_spec.rb
@@ -21,4 +21,25 @@ RSpec.describe V1::BookAPI do
       expect(result['error']).to eq('Record Not Found')
     end
   end
+
+  context 'GET /api/v1/book_price_quantity' do
+    it 'should return 200 and books' do
+      params = { price: 10, quantity: 3 }
+      get '/api/v1/book_price_quantity', params: params
+      result = JSON.parse(response.body)
+
+      expect(response.status).to eq(200)
+      expect(result.first['name']).to eq('A Whole New World Bookstore')
+      expect(result.first['books'].size).to eq(12)
+    end
+
+    it 'should return 404 and not found' do
+      params = { price: 5, quantity: 10 }
+      get '/api/v1/book_price_quantity', params: params
+      result = JSON.parse(response.body)
+
+      expect(response.status).to eq(404)
+      expect(result['error']).to eq('Record Not Found')
+    end
+  end
 end
