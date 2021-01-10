a61849f0d17797ac94430582e7e81517d93dea2b
willy chang <willy.chang@oakda.io>
2021-01-10 14:13:35 +0800

    task 1, 2 done !

--- app/api/v1/store_api.rb
+++ app/api/v1/store_api.rb
@@ -1,8 +1,60 @@
 module V1
   class StoreAPI < Base
-    desc 'the api for store'
-    get '/store_all' do
-      Store.all
+    helpers do
+      def not_found_method(result)
+        return error!('Record Not Found', 404) unless result.present?
+      end
+    end
+
+    desc 'Store certain datetime'
+    params do
+      requires :open_time, type: String, desc: 'ex: 3pm or 5:30am'
+    end
+
+    post '/store_certain_time' do
+      result = []
+      Store.all.each do |s|
+        s.opening_hours.each do |ss|
+          result << s if ss[:open] == params[:open_time]
+        end
+      end
+      not_found_method(result)
+
+      result
+    end
+
+    desc 'Store open weekday and time'
+    params do
+      requires :day, type: String, desc: 'ex: Mon or Fri'
+    end
+
+    post '/store_open_day' do
+      result = []
+      Store.all.each do |s|
+        s.opening_hours.each do |ss|
+          result << ss.merge({ store_name: s.name }) if ss[:day] == params[:day]
+        end
+      end
+      not_found_method(result)
+
+      result
+    end
+
+    desc 'Store opening hours'
+    params do
+      requires :day, type: String, desc: 'ex: Mon or Fri'
+    end
+
+    post '/store_open_day' do
+      result = []
+      Store.all.each do |s|
+        s.opening_hours.each do |ss|
+          result << ss.merge({ store_name: s.name }) if ss[:day] == params[:day]
+        end
+      end
+      not_found_method(result)
+
+      result
     end
   end
 end
--- spec/api/v1/store_api_spec.rb
+++ spec/api/v1/store_api_spec.rb
@@ -1,13 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe V1::StoreAPI do
-  context 'GET /api/v1/store_all' do
-    it 'should return 200 and store' do
-      get '/api/v1/store_all'
-      VCR.use_cassette('test') do
-        result = JSON.parse(response.body)
-        expect(response.status).to eq(200)
-      end
+  context 'POST /api/v1/store_certain_time' do
+    it 'should return 201 and store' do
+      params = { open_time: '5:30am' }
+      post '/api/v1/store_certain_time', params: params
+      result = JSON.parse(response.body)
+
+      expect(response.status).to eq(201)
+      expect(result.first['name']).to eq('Pick-a-Book')
+      expect(result.first['cash_balance']).to eq('416.69')
+    end
+
+    it 'should return 404 and not found' do
+      params = { open_time: 'test' }
+      post '/api/v1/store_certain_time', params: params
+      result = JSON.parse(response.body)
+
+      expect(response.status).to eq(404)
+      expect(result['error']).to eq('Record Not Found')
+    end
+  end
+
+  context 'POST /api/v1/store_open_day' do
+    it 'should return 201 and result' do
+      params = { day: 'Fri' }
+      post '/api/v1/store_open_day', params: params
+      result = JSON.parse(response.body)
+
+      expect(response.status).to eq(201)
+      expect(result[2]['store_name']).to eq('A Whole New World Bookstore')
+      expect(result[2]['close']).to eq('9pm')
+    end
+
+    it 'should return 404 and not found' do
+      params = { day: 'rrrrr' }
+      post '/api/v1/store_open_day', params: params
+      result = JSON.parse(response.body)
+
+      expect(response.status).to eq(404)
+      expect(result['error']).to eq('Record Not Found')
     end
   end
 end
